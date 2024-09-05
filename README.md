f24a925e4407d4f8f227e23a7418fffe585918ed
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2024-09-05 16:14:29 +0100

    Apple website clone with Gsap & Three.js

---
+++ README.md
@@ -0,0 +1,3 @@
+# A clone of Apple iPhone landing page
+
+I built this project to perfect my knowledge on animations using Gsap and Three.js following [`JSMastery youtube tutorial`](https://www.youtube.com/watch?v=kRQbRAJ4-Fs&t=1282s).
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "jaccey-code",
+    project: "apple"
+  })],
+
+  server: {
+    port: 9000,
+  },
+
+  build: {
+    sourcemap: true
+  }
+});
